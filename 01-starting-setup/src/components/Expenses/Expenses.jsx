8de7603c96a858f304fe5a2eb471c8ed2bcfa20a
haewoong_lee <haewoong_lee@tmax.co.kr>
2023-10-24 19:57:02 +0900

    commit

--- 01-starting-setup/src/components/Expenses/Expenses.jsx
+++ 01-starting-setup/src/components/Expenses/Expenses.jsx
@@ -1,19 +1,32 @@
 import ExpenseItem from './Expenseltem';
+
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props) {
+  const saveExpensesHandler = (year) => {
+    const newExpenses = props.expenses.filter(
+      (expense) => String(expense.date.getFullYear()) === year
+    );
+
+    props.onFilterExpenses(newExpenses);
+  };
+
   return (
-    <Card className="expenses">
-      <h2>Let's get started!</h2>
-      {props.expenses.map((expense, index) => (
-        <ExpenseItem
-          key={index}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
-    </Card>
+    <div>
+      <ExpensesFilter onFilterExpenses={saveExpensesHandler} />
+      <Card className='expenses'>
+        <h2>Let's get started!</h2>
+        {props.expenses.map((expense, index) => (
+          <ExpenseItem
+            key={index}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
   );
 }
 
